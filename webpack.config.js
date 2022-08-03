055ac693ba7aa5619555f81ec7530d65f15a73d9
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-03 10:39:44 -0500

    Webpack integration

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.bundle.js",
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: "body",
+      meta: { "theme-color": "#70A3CC" },
+      favicon: "./src/weather.svg",
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ccs$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
